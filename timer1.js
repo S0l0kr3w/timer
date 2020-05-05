4f446c30a887509c0c3de934a12ebb18768c7944
Jason Junio <jjjunio@gmail.com>
2020-05-05 19:23:01 +0000

    created timer.js function

---
+++ timer1.js
@@ -0,0 +1,23 @@
+const sorter = (a, b) => (a - b)
+const seconds = process.argv.slice(2).sort(sorter)
+
+
+
+const timer = function(seconds) {
+  
+  for(let time in seconds) {
+
+    setTimeout(() => {
+      process.stdout.write('.');;
+    }, 1000 * seconds[time]);
+
+  }
+};
+
+timer(seconds);
+
+// For this app, we can think of at least these three:
+
+// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
